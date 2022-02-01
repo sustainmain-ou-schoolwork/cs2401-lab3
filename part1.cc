076788bc16de282447d454072bcb50196da504dd
Josh Marusek <jm553020@ohio.edu>
2022-02-01 16:46:02 -0500

    finish part 1

--- part1.cc
+++ part1.cc
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 
-void pretty( ){ 
-	static int x = 0;
+void pretty( ) { 
+	int x = 0;
     x++; 
 
-    for(int i = 0; i < x; ++i){ 
+    for(int i = 0; i < x; ++i) { 
         cout << "*"; 
     } 
 
     cout << endl;
 } 
 
-int main(){
-    for(int i = 0; i < 6; i++){
+int main() {
+    for(int i = 0; i < 6; i++) {
         pretty();
     }
 }
